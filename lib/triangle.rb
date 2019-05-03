1ee6014af45d30eb0ad73cd215c99b5ef517d7c5
Anthony Rurup <tcrurup@gmail.com>
2019-05-03 17:50:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,12 @@ class Triangle
   end
   
   def kind
-    if self.side_lengths
+    #The different types of triangles can be determined by how many unique inegers are 
+    #in the array of side_lengths. 1 unique is an equalaterial, 2 unique is an isoscele, and 
+    #3 unique is a scalene
+    
+    
+    
   end
   
   

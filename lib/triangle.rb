3b5620db94c0e316a98a849b8d9fcee6ddf8758c
Anthony Rurup <tcrurup@gmail.com>
2019-05-03 17:52:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,7 @@ class Triangle
   attr_reader :side_lengths
   
   def initialize(side_length_1, side_length_2, side_length_3)
-    @side_length = [side_length_1, side_length_2, side_length_3]
+    @side_lengths = [side_length_1, side_length_2, side_length_3]
   end
   
   def kind

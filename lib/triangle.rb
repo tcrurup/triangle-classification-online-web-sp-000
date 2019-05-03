79304405e805c9b175df47986e4d6ae88e5a4a7e
Anthony Rurup <tcrurup@gmail.com>
2019-05-03 17:53:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
     #in the array of side_lengths. 1 unique is an equalaterial, 2 unique is an isoscele, and 
     #3 unique is a scalene
     
-    case self.side_lengths.length
+    case self.side_lengths.uniq.length
       when 1 
         :equilateral
       when 2 

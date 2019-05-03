d62161a10dbdd0253202d90a5e2b2f805c25e69c
Anthony Rurup <tcrurup@gmail.com>
2019-05-03 17:41:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,8 @@
 class Triangle
-  # write code here
+  
+  def initialize(triangle_side_length_1, triangle_side_length_2, triangle_side_length_3)
+    @side_length_1 = triangle_side_length_1
+    @side_length_2 = triangle_side_length_2
+    @side_length_3 = triangle_side_length_3
+  end
 end
